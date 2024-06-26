1a2460ec795757a2965b0db77cbbbfc4f06fd6fc
Erka <Erka.Laqi@fti.edu.al>
2024-06-26 18:08:24 +0200

    all project

--- resources/views/purchases/index.blade.php
+++ resources/views/purchases/index.blade.php
@@ -2,7 +2,7 @@
 @section('title','dashboard')
 @section('content-header','Dashboard')
 @section('content-action')
-    {{--<a href="{{route('purchases.create')}}" class="btn btn-primary mt-2 mt-sm-0 btn-icon-text"><i class="mdi mdi-plus-circle"></i>Add New</a>--}}
+   
 @endsection
 @section('content')
     <div>Index</div>
